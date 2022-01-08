bfc72bfa4415770a0dee15aa5f2a08bfffc5be4e
BlackToona <christinebgang@gmail.com>
2022-01-08 18:39:42 +1100

    q7unfinished

---
+++ src/routes/survey/page7.svelte
@@ -0,0 +1,37 @@
+
+<!-- started to work on this 8Jan, needs review -->
+    <div>
+        7a. Number of people who usually live at this property? 
+	<ul>
+		<li>
+			<input type="number" id="1" name="1" unchecked />
+			<label for="1">0-18 years</label>
+		</li>
+		<li>
+			<input type="number" id="2" name="2" unchecked />
+			<label for="2">19-50 years</label>
+		</li>
+		<li>
+			<input type="number" id="3" name="3" unchecked />
+			<label for="3">51-70 years</label>
+		</li>
+		<li>
+			<input type="number" id="4" name="4" unchecked />
+			<label for="4">70+ years</label>
+		</li>
+	
+	</ul>
+
+</div>
+
+<div>
+	7b. Do you consider any person on the property to be vulnerable ?
+	<ul>
+		<li>
+			<input type="checkbox" id="5" name="5" unchecked />
+			<label for="5">Yes</label>
+		</li>
+		<li>
+			<input type="checkbox" id="6" name="6" unchecked />
+			<label for="6">No</label>
+</div>
