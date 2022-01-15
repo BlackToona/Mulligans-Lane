acc59f10b4fea39bbb680582c1fb784ac63f28f1
BlackToona <christinebgang@gmail.com>
2022-01-15 13:32:00 +1100

    lastone

--- src/routes/survey/pagefinal.svelte
+++ src/routes/survey/pagefinal.svelte
@@ -7,14 +7,14 @@
 <p2> Prepare Connect Rebound</p2>
 <br/>
 <p> Thank you for your time in completing the survey.  <br/>
-    The information you have provided will be used to create a digital "map" of the area 
+    The information you have provided will be used to create a digital "map" of the area <br/>
     providing information which will be useful to emergency responders in time of need. <br/>
 
-    We will keep you up to date with progress on the project through 
-    our email mailouts, the Burrell Creek Notes and Facebook posts. <br/>
+    We will keep you up to date with progress on the project through <br/>
+    our email mailouts, the Burrell Creek Notes and Facebook posts. <br/> <br/>
     
-    If you have any suggestions or questions don't hesitate to contact 
-    either Helen on  0424 515 963 or Christine on 0488 288 661. 
+    If you have any suggestions or questions don't hesitate to contact <br/>
+    either Helen on  0424 515 963 or Christine on 0488 288 661. <br/>
     
     <br/></p>
     <p3>With your help we can build a safer, connected community for us all.</p3><br/>
