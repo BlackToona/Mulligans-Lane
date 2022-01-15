f877baedae811157b1321121446c5359562f5b52
BlackToona <christinebgang@gmail.com>
2022-01-15 13:28:20 +1100

    finalpgupdate

--- src/routes/survey/pagefinal.svelte
+++ src/routes/survey/pagefinal.svelte
@@ -1,3 +1,44 @@
 
 
+<div class="container">
+   
+
+<br/>
+<p2> Prepare Connect Rebound</p2>
+<br/>
+<p> Thank you for your time in completing the survey.  <br/>
+    The information you have provided will be used to create a digital "map" of the area 
+    providing information which will be useful to emergency responders in time of need. <br/>
+
+    We will keep you up to date with progress on the project through 
+    our email mailouts, the Burrell Creek Notes and Facebook posts. <br/>
+    
+    If you have any suggestions or questions don't hesitate to contact 
+    either Helen on  0424 515 963 or Christine on 0488 288 661. 
+    
+    <br/></p>
+    <p3>With your help we can build a safer, connected community for us all.</p3><br/>
+    <p>Thanks for your time and comments.</p>
+<br/>
+</div>
+
+ 
+<style>
+    .container{
+        margin: 40px, 60px;
+        background-color: papayawhip;
+        text-align: center;}
+
+    
+    p2{font-size: x-large;
+        font-weight: bold;
+            color:red}
+   
+    p3{font-size: large;
+        font-weight: bold;
+        text-align: center;
+        color:red}
+    
+    </style>
+
  
